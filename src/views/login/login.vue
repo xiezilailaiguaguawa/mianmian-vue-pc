bb57be12886d5e2956fb3608119a9cd7f3387337
xiezilailaiguaguawa <852597720@qq.com>
2021-12-11 04:12:55 +0800

    布局完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,54 @@
 <template>
   <div class="login-container">
     <!-- 左侧 from表单 -->
-    <div class="from-wrapper"></div>
+    <div class="from-wrapper">
+      <!-- 标题 -->
+      <div class="title-box">
+        <img class="login-logo" src="../../assets/login_icon.png" alt="" />
+        <span class="title">黑马面面</span>
+        <span class="line"></span>
+        <span class="sub-title">用户登录</span>
+      </div>
+      <!-- 输入框 -->
+      <el-input
+        class="phone-input"
+        placeholder="请输入手机号"
+        prefix-icon="el-icon-user"
+        v-model="input2"
+      >
+      </el-input>
+      <el-input
+        class="password-input"
+        placeholder="请输入密码"
+        prefix-icon="el-icon-lock"
+        v-model="input2"
+      >
+      </el-input>
+      <el-row class="captcha-row">
+        <el-col :span="17"
+          ><el-input
+            placeholder="请输入验证码"
+            prefix-icon="el-icon-key"
+            v-model="input2"
+          >
+          </el-input
+        ></el-col>
+        <el-col :span="7">
+          <img class="captcha" src="../../assets/login_captcha.png" alt="" />
+        </el-col>
+      </el-row>
+
+      <!-- 协议 -->
+      <el-checkbox class="checkbox">
+        我已阅读并同意
+        <el-link type="primary">用户协议</el-link>
+        和
+        <el-link type="primary">隐私条款</el-link>
+      </el-checkbox>
+      <!-- 登录 注册按钮 -->
+      <el-button class="login-btn" type="primary">登录</el-button>
+      <el-button class="reg-btn" type="primary">注册</el-button>
+    </div>
     <!-- 右侧 图片 -->
     <img src="../../assets/login_bg.png" alt="" class="banner" />
   </div>
@@ -13,7 +60,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less">
 .login-container {
   display: flex;
   /* 上下居中 */
@@ -21,11 +68,74 @@ export default {
   /* 水平对齐方式 */
   justify-content: space-around;
   height: 100%;
-  background:linear-gradient(225deg,rgba(20,147,250,1),rgba(1,198,250,1));
-}
-.from-wrapper {
-  width: 478px;
-  height: 550px;
-  background-color: #f5f5f5;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  .from-wrapper {
+    width: 478px;
+    height: 550px;
+    background-color: #f5f5f5;
+    padding-top: 48px;
+    padding-left: 43px;
+    padding-right: 41px;
+    box-sizing: border-box;
+    .title-box {
+      display: flex;
+      align-items: center;
+      .login-logo {
+        width: 22px;
+        height: 17px;
+        margin-right: 14px;
+      }
+      .title {
+        font-size: 24px;
+        margin-right: 12px;
+      }
+      .line {
+        width: 1px;
+        height: 28px;
+        background: rgba(199, 199, 199, 1);
+        margin-right: 14px;
+      }
+      .sub-title {
+        font-size: 22px;
+      }
+    }
+    .phone-input {
+      margin-top: 30px;
+    }
+    .password-input {
+      margin-top: 25px;
+    }
+    .captcha-row {
+      margin-top: 25px;
+      .captcha {
+        // width: 110px;
+        width: 100%;
+        height: 42px;
+      }
+    }
+    // 协议
+    .checkbox {
+      margin-top: 32px;
+      color: #999;
+      font-size: 14px;
+      display: flex;
+      align-items: center;
+      .el-checkbox_label {
+        display: flex;
+        align-items: center;
+      }
+    }
+    // 按钮
+    .login-btn,
+    .reg-btn {
+      width: 100%;
+      margin-left: 0;
+      margin-top: 27px;
+    }
+  }
 }
 </style>
